b389d207bd47007f1beb90aebfb9812fb7f75fd6
Labyrintos <Labyrintos71@gmail.com>
2019-10-11 16:38:29 +0900

    [수정]RetrofitActivity 모듈화

--- JavaModule/app/src/main/java/java/module/labyrintos/Retrofit/RetrofitActivity.java
+++ JavaModule/app/src/main/java/java/module/labyrintos/Retrofit/RetrofitActivity.java
@@ -21,6 +21,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class RetrofitActivity extends AppCompatActivity {
     String TAG = RetrofitActivity.class.getSimpleName();
+    private RetrofitService retrofitService;
+
     //https://reqres.in/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +36,7 @@ public class RetrofitActivity extends AppCompatActivity {
 
         Retrofit.Builder retrofitBuilder = new Retrofit.Builder().baseUrl("https://reqres.in/");
 
-        if(BuildConfig.DEBUG) {
+        if (BuildConfig.DEBUG) {
             retrofitBuilder.client(client);
         }
         retrofitBuilder.addConverterFactory(GsonConverterFactory.create());
@@ -46,65 +48,72 @@ public class RetrofitActivity extends AppCompatActivity {
                 //.addConverterFactory(ScalarsConverterFactory.create())  // String 등 처리시
                 .build();
 */
+        retrofitService = retrofit.create(RetrofitService.class);
 
-        RetrofitService retrofitService = retrofit.create(RetrofitService.class);
-        Call<PostData> tickerInfo = retrofitService.doLogin("eve.holt@reqres.in","cityslicka");
+
+    }
+
+    private void testPOST() {
+        Call<PostData> tickerInfo = retrofitService.doLogin("eve.holt@reqres.in", "cityslicka");
         tickerInfo.enqueue(new Callback<PostData>() {
             @Override
             public void onResponse(Call<PostData> call, Response<PostData> response) {
-                if(response.body()!=null)
+                if (response.body() != null)
                     Toast.makeText(RetrofitActivity.this, response.body().token, Toast.LENGTH_SHORT).show();
             }
+
             @Override
             public void onFailure(Call<PostData> call, Throwable t) {
                 Toast.makeText(RetrofitActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
             }
         });
-/*
+    }
 
-        RetrofitService retrofitService = retrofit.create(RetrofitService.class);
-        Call<PutData> tickerInfo = retrofitService.updateJob(2,"morpheus","zion resident");
+    private void testPUT() {
+        Call<PutData> tickerInfo = retrofitService.updateJob(2, "morpheus", "zion resident");
         tickerInfo.enqueue(new Callback<PutData>() {
             @Override
             public void onResponse(Call<PutData> call, Response<PutData> response) {
-                if(response.body()!=null)
-                Toast.makeText(RetrofitActivity.this, response.body().updatedAt, Toast.LENGTH_SHORT).show();
+                if (response.body() != null)
+                    Toast.makeText(RetrofitActivity.this, response.body().updatedAt, Toast.LENGTH_SHORT).show();
             }
+
             @Override
             public void onFailure(Call<PutData> call, Throwable t) {
                 Toast.makeText(RetrofitActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
             }
         });
-*/
-
-        // Call<ListUser> tickerInfo = retrofitService.users(2);
-        //        tickerInfo.enqueue(new Callback<ListUser>() {
-        //            @Override
-        //            public void onResponse(Call<ListUser> call, Response<ListUser> response) {
-        //                if(response.body()!=null)
-        //                Toast.makeText(RetrofitActivity.this, response.body().user.get(0).avatar, Toast.LENGTH_SHORT).show();
-        //            }
-        //            @Override
-        //            public void onFailure(Call<ListUser> call, Throwable t) {
-        //                Toast.makeText(RetrofitActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
-        //            }
-        //        });
-
-        //Call<SingleUser> tickerInfo = openApiService.user(2);
-        //        tickerInfo.enqueue(new Callback<SingleUser>() {
-        //            @Override
-        //            public void onResponse(Call<SingleUser> call, Response<SingleUser> response) {
-        //                if(response.body()!=null)
-        //                Toast.makeText(RetrofitActivity.this, response.body().user.avatar, Toast.LENGTH_SHORT).show();
-        //            }
-        //            @Override
-        //            public void onFailure(Call<SingleUser> call, Throwable t) {
-        //                Toast.makeText(RetrofitActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
-        //            }
-        //        });
-
-
-
     }
 
+    private void testGET2() {
+        Call<ListUser> tickerInfo = retrofitService.users(2);
+        tickerInfo.enqueue(new Callback<ListUser>() {
+            @Override
+            public void onResponse(Call<ListUser> call, Response<ListUser> response) {
+                if (response.body() != null)
+                    Toast.makeText(RetrofitActivity.this, response.body().user.get(0).avatar, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<ListUser> call, Throwable t) {
+                Toast.makeText(RetrofitActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void testGET1() {
+        Call<SingleUser> tickerInfo = retrofitService.user(2);
+        tickerInfo.enqueue(new Callback<SingleUser>() {
+            @Override
+            public void onResponse(Call<SingleUser> call, Response<SingleUser> response) {
+                if (response.body() != null)
+                    Toast.makeText(RetrofitActivity.this, response.body().user.avatar, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<SingleUser> call, Throwable t) {
+                Toast.makeText(RetrofitActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
