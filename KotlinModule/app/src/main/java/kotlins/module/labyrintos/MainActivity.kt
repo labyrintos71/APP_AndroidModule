a6de6294ddf7bab00c2efbd00c04a3f763e68f78
Labyrintos <labyrintos71@gmail.com>
2019-10-24 00:01:15 +0900

    [수정] anko layout 세팅

--- KotlinModule/app/src/main/java/kotlins/module/labyrintos/MainActivity.kt
+++ KotlinModule/app/src/main/java/kotlins/module/labyrintos/MainActivity.kt
@@ -3,20 +3,22 @@ package kotlins.module.labyrintos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.yesButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val test = test()
-        test.tests=2
-        text.text= test.tests.toString() +""
-        assert(test.tests>3)
-        assert(false) {"123"}
-        throw AssertionError("123")
-    }
-    class test{
-        var tests=1
+        alert ("다이얼로그 메세지 데숭", "다이얼로그" ){
+            yesButton { toast("OK 누름") }
+            noButton { toast("cancel을 클릭") }
+        }
+     //   assert(test.tests>3)
+     //   assert(false) {"123"}
+     //   throw AssertionError("123")
     }
 }
