5224bb6ea81467c5edc845c527a57540c8c84448
Labyrintos <labyrintos71@gmail.com>
2019-10-26 20:55:47 +0900

    [수정] anko 예제추가

--- KotlinModule/app/src/main/java/kotlins/module/labyrintos/MainActivity.kt
+++ KotlinModule/app/src/main/java/kotlins/module/labyrintos/MainActivity.kt
@@ -2,8 +2,12 @@ package kotlins.module.labyrintos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.*
+import org.jetbrains.anko.design.R
+import org.jetbrains.anko.design.floatingActionButton
+import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +15,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         verticalLayout{
-            editText{
-                onClick {
+            padding = dip(20)
 
-                }
-                hint="asdf"
-
+            calendarView {  }.lparams(width = matchParent, height = wrapContent)
+            floatingActionButton {
+                imageResource = R.drawable.abc_btn_check_material
+                onClick { snackbar("안녕!") }
+            }.lparams(width = wrapContent, height = wrapContent){
+                gravity = Gravity.BOTTOM or Gravity.END
+                margin = dip(10)
             }
         }
         alert ("다이얼로그 메세지 데숭", "다이얼로그" ){
