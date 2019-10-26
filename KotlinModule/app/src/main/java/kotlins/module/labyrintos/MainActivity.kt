f925234838d849a83de92f8e99f538b7d2351b47
Labyrintos <labyrintos71@gmail.com>
2019-10-26 19:07:07 +0900

    [추가]anko layout 테스트

--- KotlinModule/app/src/main/java/kotlins/module/labyrintos/MainActivity.kt
+++ KotlinModule/app/src/main/java/kotlins/module/labyrintos/MainActivity.kt
@@ -3,16 +3,22 @@ package kotlins.module.labyrintos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.alert
-import org.jetbrains.anko.noButton
-import org.jetbrains.anko.toast
-import org.jetbrains.anko.yesButton
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        verticalLayout{
+            editText{
+                onClick {
+
+                }
+                hint="asdf"
+
+            }
+        }
         alert ("다이얼로그 메세지 데숭", "다이얼로그" ){
             yesButton { toast("OK 누름") }
             noButton { toast("cancel을 클릭") }
