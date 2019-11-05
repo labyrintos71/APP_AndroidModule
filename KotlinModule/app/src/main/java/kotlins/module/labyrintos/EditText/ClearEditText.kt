8bc6afc11beb18c16eabc6ab450b86d0eb0ef682
Labyrintos <labyrintos71@gmail.com>
2019-11-06 00:02:47 +0900

    [수정]init 추가

--- KotlinModule/app/src/main/java/kotlins/module/labyrintos/EditText/ClearEditText.kt
+++ KotlinModule/app/src/main/java/kotlins/module/labyrintos/EditText/ClearEditText.kt
@@ -1,28 +1,43 @@
 package kotlins.module.labyrintos.EditText
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.widget.AppCompatEditText
-import org.jetbrains.anko.attr
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 
 /**
  * Created by Labyrintos on 2019-11-04
  */
 class ClearEditText : AppCompatEditText, TextWatcher, View.OnTouchListener,
     View.OnFocusChangeListener {
-    var
+    private val drawable: Drawable
 
 
     init {
-
+        drawable = DrawableCompat.wrap(
+            ContextCompat.getDrawable(
+                context,
+                androidx.appcompat.R.drawable.abc_ic_clear_material
+            )!!
+        )
+        DrawableCompat.setTintList(drawable, hintTextColors)
+        drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+        setCl
     }
-    constructor(context: Context) : super(context){}
+
+    constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, def: Int) : super(context, attrs, def)
+
+    private fun setClearIconVisible(boolean: Boolean){
+
+    }
 
     override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
