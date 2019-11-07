cf6e4bbbdb3120d6f7ac132c37f5065f45b644df
Labyrintos <labyrintos71@gmail.com>
2019-11-08 01:38:41 +0900

    [추가] 수직 프로그래스바, 코틀린 포팅 필요

---
+++ KotlinModule/app/src/main/java/kotlins/module/labyrintos/Progressbar/VerticalProgressBar.java
@@ -0,0 +1,48 @@
+package kotlins.module.labyrintos.Progressbar;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.ProgressBar;
+
+import androidx.annotation.RequiresApi;
+
+/**
+ * Created by Labyrintos on 2019-11-08
+ */
+public class VerticalProgressBar extends ProgressBar {
+    public VerticalProgressBar(Context context) {
+        super(context);
+    }
+
+    public VerticalProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VerticalProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public VerticalProgressBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(h, w, oldh, oldw);
+    }
+
+    @Override
+    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected synchronized void onDraw(Canvas canvas) {
+        canvas.rotate(-90);
+        canvas.translate(-getHeight(), 0);
+        super.onDraw(canvas);
+    }
+}
