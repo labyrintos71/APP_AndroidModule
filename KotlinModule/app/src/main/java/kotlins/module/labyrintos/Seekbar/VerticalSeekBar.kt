bb3d5f7c2b06d00beda8b6c07e36d4f6edfb2b5c
Labyrintos <labyrintos71@gmail.com>
2019-11-09 00:53:52 +0900

    [수정] 불필요한 소스 제거

--- KotlinModule/app/src/main/java/kotlins/module/labyrintos/Seekbar/VerticalSeekBar.kt
+++ KotlinModule/app/src/main/java/kotlins/module/labyrintos/Seekbar/VerticalSeekBar.kt
@@ -16,57 +16,13 @@ import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatSeekBar
 import kotlins.module.labyrintos.R
+import org.jetbrains.anko.layoutInflater
 import org.jetbrains.anko.sdk27.coroutines.onSeekBarChangeListener
 
 /**
  * Created by Labyrintos on 2019-11-08
  */
 class VerticalSeekBar : AppCompatSeekBar {
-    private val DISTANCE_MIN = 0
-    private val DISTANCE_MAX = 2000
-    private val DISTANCE_STEP = 50
-
-    private val thumbView by lazy {
-        LayoutInflater.from(context).inflate(R.layout.item_seekbar_thumb, rootView as ViewGroup, true)
-    }
-    private val thumbTextView by lazy {
-        thumbView.findViewById<TextView>(R.id.thumb_progress_text)
-    }
-    init {
-        setOnSeekBarChangeListener(object :OnSeekBarChangeListener{
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                updateThumb(progress * DISTANCE_STEP)
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-            }
-        })
-        Handler().postDelayed({
-            max = (DISTANCE_MAX - DISTANCE_MIN) / DISTANCE_STEP
-            progress = 1500 / DISTANCE_STEP
-            updateThumb(progress  * DISTANCE_STEP)
-        }, 500)
-    }
-
-    private fun updateThumb(progress: Int) {
-        val thumb = progressDrawable.bounds
-        val height = thumb.right * (1 - progress.toFloat() / (max * DISTANCE_STEP).toFloat())
-
-        thumbView.layoutParams = RelativeLayout.LayoutParams(
-            RelativeLayout.LayoutParams.WRAP_CONTENT,
-            RelativeLayout.LayoutParams.WRAP_CONTENT
-        ).apply {
-            addRule(RelativeLayout.LEFT_OF, id)
-            setMargins(0, top + height.toInt(), 10, 0)
-        }
-        thumbTextView.text = "${progress}M"
-    }
-
-
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
