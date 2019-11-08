61a46d609a1b9cc9113bc0deb3e2ac8e5e6568cb
Labyrintos <labyrintos71@gmail.com>
2019-11-09 01:34:12 +0900

    [추가] 옆에 상태바 추가

--- KotlinModule/app/src/main/java/kotlins/module/labyrintos/MainActivity.kt
+++ KotlinModule/app/src/main/java/kotlins/module/labyrintos/MainActivity.kt
@@ -1,6 +1,5 @@
 package kotlins.module.labyrintos
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,17 +9,9 @@ import io.reactivex.disposables.CompositeDisposable
 class MainActivity : AppCompatActivity() {
     lateinit var compositeDisposable: CompositeDisposable
 
-    private val thumbView by lazy {
-        LayoutInflater.from(this).inflate(R.layout.item_seekbar_thumb, null, false)
-    }
-    private val thumbTextView by lazy {
-        thumbView.findViewById<TextView>(R.id.thumb_progress_text)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        layoutInflater
 
    /*     verticalLayout{
             padding = dip(20)
