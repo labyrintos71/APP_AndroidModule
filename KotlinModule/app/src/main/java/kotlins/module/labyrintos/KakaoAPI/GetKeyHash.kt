e69361e4a07ee4bd4f9cbf72978a83d2104a694a
Labyrintos <labyrintos71@gmail.com>
2019-11-10 00:08:45 +0900

    [추가] keyhash 얻어오는소스 추가

---
+++ KotlinModule/app/src/main/java/kotlins/module/labyrintos/KakaoAPI/GetKeyHash.kt
@@ -0,0 +1,28 @@
+package kotlins.module.labyrintos.KakaoAPI
+
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlins.module.labyrintos.Permission.ExamActivity
+import java.security.MessageDigest
+
+/**
+ * Created by Labyrintos on 2019-11-09
+ */
+
+class GetKeyHash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= 28){
+            val info = packageManager.getPackageInfo("kotlins.module.labyrintos", PackageManager.GET_SIGNING_CERTIFICATES)
+            for(infos in info.signingInfo.apkContentsSigners){
+                val md = MessageDigest.getInstance("SHA")
+                md.update(infos.toByteArray())
+                Log.e("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            }
+        }
+    }
+}
