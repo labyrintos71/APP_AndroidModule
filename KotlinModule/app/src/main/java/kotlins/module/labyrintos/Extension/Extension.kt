e9b3b70d9d422134eca7087706e2a602b17b64da
Labyrintos <Labyrintos71@gmail.com>
2019-11-12 22:38:07 +0900

    [추가] multiple with

--- KotlinModule/app/src/main/java/kotlins/module/labyrintos/Extension/Extension.kt
+++ KotlinModule/app/src/main/java/kotlins/module/labyrintos/Extension/Extension.kt
@@ -1,8 +1,16 @@
 package kotlins.module.labyrintos.Extension
 
+import android.content.Context
+
 /**
  * Created by Labyrintos on 2019-11-11
  */
 
-fun Int.dpToPx(): Int = (this * context.resources.displayMetrics.density).toInt()
-fun Int.pxToDp(): Int = (this / context.resources.displayMetrics.density).toInt()
+fun Int.dpToPx(context: Context): Int = (this * context.resources.displayMetrics.density).toInt()
+fun Int.pxToDp(context: Context): Int = (this / context.resources.displayMetrics.density).toInt()
+
+fun <T> multipleWith(vararg receivers: T, block: T.() -> Unit) {
+    receivers.map {
+        it.block()
+    }
+}
